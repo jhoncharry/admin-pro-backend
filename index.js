b72512f54eb486b0ad60531d93cd7dbf56eb3130
Jhon <jhon.charryserrato@gmail.com>
2020-11-08 19:28:43 -0500

    CORS Y Express - primer commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const cors = require('cors');
+
+
+const express = require('express');
+const { dbConnection } = require('./database/config');
+
+
+// Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use(cors())
+
+// Base de datos
+dbConnection();
+
+
+
+// Rutas
+app.get('/', (req, res) => {
+
+    res.status(400).json({
+        ok: true,
+        msg: "Hola mundo"
+    });
+
+});
+
+
+
+
+app.listen(process.env.PORT, () => {
+
+    console.log("Servidor corriendo en puerto " + process.env.PORT);
+
+});
