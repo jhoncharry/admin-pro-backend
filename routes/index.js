f6e8d8cc129db0361c7533566c61110fa23c4fb2
Jhon <jhon.charryserrato@gmail.com>
2021-01-05 21:27:51 -0500

    SPA control

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 
 app.use(require('./usuarios'));
 app.use(require('./login'));
@@ -8,6 +10,12 @@ app.use(require('./hospitales'));
 app.use(require('./medicos'));
 app.use(require('./busquedas'));
 app.use(require('./uploads'));
+
+
+// Lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../public/index.html"));
+});
 
 
 
